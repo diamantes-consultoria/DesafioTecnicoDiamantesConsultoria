e9e4e9e53d3741e1d4f1cc72809c4718b87cf8ed
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-06-10 13:25:16 -0300

    chore: server init

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello, World!');
+// src/index.ts
+import express from 'express';
+import { json } from 'body-parser';
+import dotenv from 'dotenv';
+import { authRoutes } from './routes/user.router';
+import { productRoutes } from './routes/product.router';
+
+dotenv.config();
+
+const app = express();
+
+app.use(json());
+app.use('/api', authRoutes);
+app.use('/api', productRoutes);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
