b2cbefd0e78b19d8cd2ac6d30b4af5a74ec84a85
Redyf <mateusalvespereira7@gmail.com>
2024-06-19 22:51:52 -0300

    refactor: Improve responsiveness for Main component

--- components/Main.tsx
+++ components/Main.tsx
@@ -3,34 +3,39 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div className="text-black p-4 flex flex-col items-center md:flex-row md:justify-start md:items-start md:p-8 lg:p-12">
-      <div className="w-full md:w-2/3 md:mt-0 md:mr-8">
-        <h1 className="text-4xl mt-48 md:text-6xl mb-8 md:mb-16 font-bold ">
+    <div className="text-black p-4 flex flex-col items-center md:flex-row md:justify-between md:items-start md:p-6 lg:p-8">
+      <div className="w-full md:w-2/3 md:mr-6 mb-4 md:mb-0 flex flex-col justify-between">
+        <h1 className="text-3xl md:text-5xl lg:text-6xl mb-6 md:mb-10 font-bold">
           Vista-se de elegância e <br className="hidden md:block" /> estilo -
           descubra a moda <br className="hidden md:block" /> perfeita para você
         </h1>
 
-        <ul className="text-lg md:text-2xl flex mb-4 md:mb-8">
-          <li className="mr-4 md:mr-8">
-            <span className="font-bold text-2xl md:text-4xl">+280000</span>
+        <ul className="text-base md:text-lg lg:text-2xl flex flex-col md:flex-row">
+          <li className="mr-4 md:mr-6 mb-2 md:mb-0">
+            <span className="font-bold text-xl md:text-2xl lg:text-4xl">
+              +280000
+            </span>
             <br />
             Vendas realizadas
           </li>
           <li>
-            <span className="font-bold text-2xl md:text-4xl">+40000</span>{" "}
+            <span className="font-bold text-xl md:text-2xl lg:text-4xl">
+              +40000
+            </span>
             <br />
             Produtos disponíveis
           </li>
         </ul>
       </div>
 
-      <div className="w-full mr-48 md:w-1/3 flex justify-center md:justify-end">
+      <div className="w-full md:w-1/3 flex justify-center md:justify-end md:ml-6">
         <Image
           src={"/pessoa.png"}
           alt="Imagem da pessoa"
-          width={368}
-          height={452}
-          className="rounded-lg"
+          width={300}
+          height={340}
+          className="rounded-lg object-cover"
+          priority={true}
         />
       </div>
     </div>
