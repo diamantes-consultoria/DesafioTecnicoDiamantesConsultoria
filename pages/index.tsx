0a1a1e5d315a712a634d5b73da6e587d3b518869
Redyf <mateusalvespereira7@gmail.com>
2024-06-20 14:37:29 -0300

    feat: fetch roupas from database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,59 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
-import Content from "@/components/Nav";
 import Nav from "@/components/Nav";
 import Main from "@/components/Main";
 import Bar from "@/components/Bar";
+import Roupa, { RoupaProps } from "@/components/Roupa";
 
 const inter = Inter({ subsets: ["latin"] });
 
-interface Roupa {
-  id: number;
-  nome: string;
-  preco: number;
-}
-
 export default function Home() {
-  // const [roupas, setRoupas] = useState<Roupa[]>([]);
+  const [products, setProducts] = useState<RoupaProps[]>([]);
 
-  // useEffect(() => {
-  //   const fetchRoupas = async () => {
-  //     const res = await fetch("/api/roupas");
-  //     const data = await res.json();
-  //     setRoupas(data);
-  //   };
-  //
-  //   fetchRoupas();
-  // }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("/api/products");
+        if (res.ok) {
+          const data = await res.json();
+          console.log("Fetched products:", data);
+          setProducts(data);
+        } else {
+          throw new Error("Failed to fetch products");
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <div>
       <Bar
-        text="Ganhe 25% de desconto na primeira compra"
-        textSize="text-2xl"
+        text="Ganhe 25% de desconto na primeira compra."
+        textSize="text-base md:text-lg lg:text-2xl"
+        padding="p-2 md:p-4 lg:p-10"
       />
       <Nav />
       <Main />
-      <Bar text="INNOVATION STORE" textSize="text-5xl" padding="p-16" />
-      {/* <ul> */}
-      {/*   {Array.isArray(roupas) && */}
-      {/*     roupas.map((roupa) => ( */}
-      {/*       <li key={roupa.id}> */}
-      {/*         {roupa.nome} - {roupa.preco} */}
-      {/*       </li> */}
-      {/*     ))} */}
-      {/* </ul> */}
+      <Bar
+        text="INNOVATION STORE"
+        textSize="text-base md:text-lg lg:text-4xl"
+        padding="p-4 md:p-8 lg:p-14"
+      />
+      <div className="flex flex-wrap justify-center w-full">
+        {products.map((product) => (
+          <Roupa
+            id={product.id}
+            image={product.image}
+            name={product.name}
+            rating={product.rating}
+            value={product.value}
+          />
+        ))}
+      </div>
     </div>
   );
 }
